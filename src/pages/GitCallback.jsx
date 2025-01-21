601e0fff054cdbe12008423554c9dcf580e7de32
Teophil Simao <teophilsimao@gmail.com>
2025-01-21 13:57:56 +0100

    login w git

---
+++ src/pages/GitCallback.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useUserContext } from '../context/UserContext'
+
+const GitCallback = () => {
+  const navigate = useNavigate()
+  const { login } = useUserContext()
+
+  useEffect(() => {
+    
+    const user = async () => {
+      try {
+        const storedUser = localStorage.getItem('user')
+        const storedToken = localStorage.getItem('token')
+  
+        if (storedUser && storedToken) {
+          navigate('/')
+          return
+        }
+
+        const query = new URLSearchParams(window.location.search)
+        const token = query.get('token')
+        const userStr = query.get('user')
+
+        if (!token || !userStr) {
+          navigate('/login')
+          return
+        }
+        
+        const user = JSON.parse(decodeURIComponent(userStr))
+        console.log('User Logged In', user, token)
+        login(user, token)
+        navigate('/')
+      } catch (error) {
+        console.log('Failed', error)
+        navigate('/login')
+      }
+    }
+
+    user()
+  }, [login, navigate])
+  return (
+    <div>
+      Logging in...
+    </div>
+  )
+}
+
+export default GitCallback
